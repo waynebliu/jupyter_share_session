31eadab3e5dc1d41cffa8056b3b704fc4baf6e02
waynebliu <waynebliu@yahoo.com>
2017-09-30 21:29:53 -0400

    finish pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='jupyter_share_session',
+      version='0.1',
+      description='Split a notebook into multiple files.',
+      url='http://github.com/waynebliu/jupyter_share_session/',
+      author='Wayne Liu',
+      author_email='waynebliu@yahoo.com',
+      license='MIT',
+      packages=['jupyter_share_session'],
+      include_package_data=True,
+      zip_safe=False)
+
