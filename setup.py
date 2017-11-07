47799883e163d7ed19c611cfed5269d412e8f2e7
waynebliu <waynebliu@yahoo.com>
2017-11-07 07:55:46 -0500

    update v0.2.5 - add varname for nsdefault

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='jupyter_share_session',
-      version='0.2.4',
+      version='0.2.5',
       description='Split a notebook into multiple files.',
       url='http://github.com/waynebliu/jupyter_share_session/',
       author='Wayne Liu',
